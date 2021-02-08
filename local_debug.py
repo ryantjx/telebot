d95b5dd19432f76838b96b7f6ff10ef943b94417
isbobby <bobbyclex8@gmail.com>
2021-02-08 21:05:24 +0800

    added local debug

--- local_debug.py
+++ local_debug.py
@@ -1,13 +1,47 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-from moneyGooseBot.credentials import bot_token
-from moneyGooseBot.master_mind import mainCommandHandler
+import logging
+from moneyGooseBot.master_mind import mainCommandHandler, start, error
+from moneyGooseBot.credentials import URL, reset_key, bot_token, bot_user_name
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram
+
+bot = telegram.Bot(token=bot_token)
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 def main():
-    updater = Updater(bot_token)
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(bot_token, use_context=True)
+
+    # Get the dispatcher to register handlers
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop',mainCommandHandler))
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, message_parser))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
     updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
+def message_parser(update, context):
+    return mainCommandHandler(incoming_message = context.message, telebot_instance=bot)
+
 
 if __name__ == '__main__':
     main()
